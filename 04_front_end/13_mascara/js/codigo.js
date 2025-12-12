25605a150e985d9adde2af2d5ee95c1360910a81
xXLuisinnXx <carvalhovaladaresluisguilherme@gmail.com>
2025-12-12 20:14:10 -0300

    Aula do dia 12/12/2025

--- 04_front_end/13_mascara/js/codigo.js
+++ 04_front_end/13_mascara/js/codigo.js
@@ -1,4 +1,7 @@
 const form = document.querySelector('form');
+const cpfMask = document.querySelector('#cpf');
+const telefoneMask = document.querySelector('#telefone');
+const cepMask = document.querySelector('#cep');
 
 const exibir = () => {
     // declaração de variaveis
@@ -23,7 +26,27 @@ const exibir = () => {
     form.reset();
 }
 
-form.addEventListener('submit', function(event){
+cpfMask.addEventListener('input', function() {
+    this.value = this.value.replace(/\D/g, '')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+});
+
+telefoneMask.addEventListener('input', function() {
+    this.value = this.value.replace(/\D/g, '')
+    .replace(/(\d{2})(\d)/, '($1) $2')
+    .replace(/(\d{4,5})(\d)/, '$1-$2')
+    .replace(/(-\d{4})\d+?$/, '$1');
+});
+
+cepMask.addEventListener('input', function(){
+    this.value = this.value.replace(/\D/g, '')
+    .replace(/(\d{5})(\d)/, '$1-$2')
+    .replace(/(-\d{3})\d+?$/, '$1');
+});
+
+form.addEventListener('submit', function (event) {
     event.preventDefault();
     exibir();
 })
