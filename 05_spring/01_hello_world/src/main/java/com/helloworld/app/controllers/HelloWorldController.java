63d476347af39708fac25063f9df876f39b05927
ead <ead@academico.local>
2026-01-28 21:55:51 -0300

    Aula do dia 28/01/2026

---
+++ 05_spring/01_hello_world/src/main/java/com/helloworld/app/controllers/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.helloworld.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
