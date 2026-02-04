f5824567f1af4cc42ecab3059fe9793de005fcbf
xXLuisinnXx <carvalhovaladaresluisguilherme@gmail.com>
2026-02-03 21:33:40 -0300

    Aula do dia 03/02/2026

--- 05_spring/03_eventos/src/main/java/com/eventos/app/controllers/EventosController.java
+++ 05_spring/03_eventos/src/main/java/com/eventos/app/controllers/EventosController.java
@@ -1,13 +1,36 @@
 package com.eventos.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.eventos.app.models.Evento;
+import com.eventos.app.repository.EventosRepository;
 
 @Controller
 public class EventosController {
+
+    @Autowired
+    private EventosRepository csr;
+
     @RequestMapping("/")
     public String index(){
         return "index";
+    }
+
+    @RequestMapping(value="/cadastrarEvento", method = RequestMethod.GET)
+    public String cadastrarEvento(){
+        return "cadastrar-evento";
+    }
+
+    @RequestMapping(value = "/cadastrarEvento", method = RequestMethod.POST)
+    public String cadastrarEvento(Evento evento){
+        csr.save(evento);
+        return "redirect:/cadastroSucesso";
+    }
+    @RequestMapping("/cadastroSucesso")
+    public String cadastroSucesso(){
+        return "cadastro-sucesso";
     }
 }
