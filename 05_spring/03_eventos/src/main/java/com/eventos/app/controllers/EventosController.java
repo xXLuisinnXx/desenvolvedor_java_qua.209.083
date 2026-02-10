589e20a253d7bcf78e890348bbcb1f00f7f10686
xXLuisinnXx <carvalhovaladaresluisguilherme@gmail.com>
2026-02-09 21:36:47 -0300

    Aula do dia 09/02/2026

--- 05_spring/03_eventos/src/main/java/com/eventos/app/controllers/EventosController.java
+++ 05_spring/03_eventos/src/main/java/com/eventos/app/controllers/EventosController.java
@@ -2,8 +2,10 @@ package com.eventos.app.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +14,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.eventos.app.models.Evento;
 import com.eventos.app.repository.EventosRepository;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class EventosController {
@@ -74,5 +78,13 @@ public class EventosController {
         Evento evento = csr.findByIdEvento(idEvento);
         csr.delete(evento);
         return "redirect:/";
+    }
+
+    @ModelAttribute
+    public void addActivePage(HttpServletRequest request, Model model){
+        String uri = request.getRequestURI();
+        if (uri.contains("/cadastrarEvento")) model.addAttribute("activePage" , "cadastrar");
+        else model.addAttribute("activePage","home");
+       
     }
 }
---
+++ 05_spring/04_login/src/main/java/com/login/app/DataConfig.java
@@ -0,0 +1,34 @@
+package com.login.app;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DataConfig {
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysl.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/db_login");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setPrepareConnection(true);
+
+        return adapter;
+    }
+}
